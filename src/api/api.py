4a66bc44cd2d4107a78caa8448b01a47ca31926b
DevSebi <52174534+LPSebi@users.noreply.github.com>
2022-10-12 23:20:13 +0200

    do some api things

--- src/api/api.py
+++ src/api/api.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from wsgiref import headers
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
+from constants.constants import *
+from dotenv import load_dotenv
+import os
+import aiohttp
 
+load_dotenv()
 app = FastAPI()
+templates = Jinja2Templates(directory="./src/api/templates")
+app.mount("/static", StaticFiles(directory="./src/api/static"), name="static")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 
 @app.get('/licensy')
@@ -18,5 +31,35 @@ async def tos():
 async def privacy():
     return 'Privacy Policy'
 
+
+@app.get('/licensy/login')
+async def login(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+
+@app.get('/licensy/cb')
+async def cb(request: Request, code: str):
+    return await exchange_code(request, code)
+    # return templates.TemplateResponse('cb.html', {'request': request})
+
+
+async def exchange_code(request: Request, code: str):
+    data = {
+        "client_id": DISCORD_CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": DISCORD_REDIRECT_URL
+    }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(DISCORD_API_ENDPOINT + "/oauth2/token", data=data, headers=headers) as resp:
+            resp.raise_for_status()
+            print(await resp.json())
+            return await resp.json()
+
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=80)
